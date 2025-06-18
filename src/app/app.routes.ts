c7983a2d50ff862d00c2f944cc1c952b9f412866
Omar <omar@Omars-MacBook-Air.local>
2025-06-18 14:35:35 -0600

    Add basic footer and header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 
-
 export const routes: Routes = [
   { path: '', redirectTo: 'invoices', pathMatch: 'full' },
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Footer } from './components/footer/footer';
+// import { InvoicesComponent } from './invoices.component';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
+    Header,
+    Footer
+    // InvoicesComponent
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
